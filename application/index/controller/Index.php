e424890be0cc2f83d921e6bce7b4916f62af6b5c
xw9887@163.com <xw9887@163.com>
2018-05-07 18:09:29 +0800

    首页数据填充、后台完善

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -15,8 +15,18 @@ class Index extends Controller
     }
     public function index()
     {
+          //公司简介
           $info=Db::name('company')->find(1);
           $this->assign('profile',$info);
+          //国际合作
+          $newslist=Db::name('article')->where(['cid'=>19,'sid'=>0,'status'=>1])->limit(4)->select();
+          $this->assign('newslist',$newslist);
+          //产品介绍
+          $productlist=Db::name('product')->limit(4)->select();
+          $this->assign('productlist',$productlist);
+          //视频
+          $video=Db::name('video')->order('create_time desc')->limit(1)->find();
+          $this->assign('video',$video);
         return $this->fetch();
     }
     public function testsasas(){
